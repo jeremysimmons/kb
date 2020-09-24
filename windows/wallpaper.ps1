0d710f0a8933986b0ead2eff3c2f08bea6998f01
Jeremy Simmons <jsimmons@jeremysimmons.net>
2020-09-24 08:05:25 -0600

    notes

---
+++ windows/wallpaper.ps1
@@ -0,0 +1,17 @@
+﻿$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+public class wallpaper
+{
+public const int SetDesktopWallpaper = 20;
+public const int UpdateIniFile = 0x01;
+public const int SendWinIniChange = 0x02;
+[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+public static void SetWallpaper ( string path )
+{
+SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+}
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+[wallpaper]::SetWallpaper("C:\Users\JSimmons\Pictures\wallpaper\1554280.jpg") 
