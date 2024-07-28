2f91a53de14b5d3a502e4e8acc3f5ec11476a712
Jeremy Simmons <jsimmons@jeremysimmons.net>
2024-07-28 12:07:22 -0600

    local dev

--- woocommerce/add-multiple-to-cart.php
+++ woocommerce/add-multiple-to-cart.php
@@ -8,11 +8,15 @@ https://www.webroomtech.com/woocommerce-add-multiple-products-to-cart-via-url
 // Fire before the WC_Form_Handler::add_to_cart_action callback.
 add_action( 'wp_loaded', 'webroom_add_multiple_products_to_cart', 15 );
 
+function webroom_option_woocommerce_cart_redirect_after_add($value, $option ) { return 'no'; }
+        
 function webroom_add_multiple_products_to_cart( $url = false ) {
         // Make sure WC is installed, and add-to-cart query arg exists, and contains at least one comma.
         if ( ! class_exists( 'WC_Form_Handler' ) || empty( $_REQUEST['add-to-cart'] ) || false === strpos( $_REQUEST['add-to-cart'], ',' ) ) {
                 return;
         }
+        add_filter( 'option_woocommerce_cart_redirect_after_add', 'webroom_option_woocommerce_cart_redirect_after_add', 10, 2);
+        
         // Remove WooCommerce's hook, as it's useless (doesn't handle multiple products).
         remove_action( 'wp_loaded', array( 'WC_Form_Handler', 'add_to_cart_action' ), 20 );
 
@@ -29,20 +33,19 @@ function webroom_add_multiple_products_to_cart( $url = false ) {
                 $_REQUEST['quantity'] = ! empty( $id_and_quantity[1] ) ? absint( $id_and_quantity[1] ) : 1;
 
                 if ( ++$number === $count ) {
-                        // Ok, final item, let's send it back to woocommerce's add_to_cart_action method for handling.
-                        $_REQUEST['add-to-cart'] = $product_id;
-                        return WC_Form_Handler::add_to_cart_action( $url );
+                   // Ok, final item, let's send it back to woocommerce's add_to_cart_action method for handling.
+                   $_REQUEST['add-to-cart'] = $product_id;
+                   return WC_Form_Handler::add_to_cart_action( $url );
                 }
 
-                $product_id        = apply_filters( 'woocommerce_add_to_cart_product_id', absint( $product_id ) );
-                $was_added_to_cart = false;
-                $adding_to_cart    = wc_get_product( $product_id );
+                $product_id = apply_filters( 'woocommerce_add_to_cart_product_id', absint( $product_id ) );
+                $product_for_cart = wc_get_product( $product_id );
 
-                if ( ! $adding_to_cart ) {
+                if ( ! $product_for_cart ) {
                         continue;
                 }
 
-                $add_to_cart_handler = apply_filters( 'woocommerce_add_to_cart_handler', $adding_to_cart->get_type(), $adding_to_cart );
+                $add_to_cart_handler = apply_filters( 'woocommerce_add_to_cart_handler', $product_for_cart->get_type(), $product_for_cart );
                 
                 // Variable product handling
                 if ( 'variable' === $add_to_cart_handler ) {
@@ -58,6 +61,7 @@ function webroom_add_multiple_products_to_cart( $url = false ) {
                         woo_hack_invoke_private_method( 'WC_Form_Handler', 'add_to_cart_handler_simple', $product_id );
                 }
         }
+        remove_filter( 'option_woocommerce_cart_redirect_after_add', 'webroom_option_woocommerce_cart_redirect_after_add', 10, 2);
 }
 
 /**
